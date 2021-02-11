2410818298a0a591cd7a925df3462857a5011403
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-11 15:07:08 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arep/app/web/WebServiceImage.java
+++ src/main/java/edu/escuelaing/arep/app/web/WebServiceImage.java
@@ -9,6 +9,6 @@ public class WebServiceImage {
      */
     @Web("/image.html")
     public static String showImage() {
-        return "<html><body><img src=\"/Wallpaper.jpg\" width=\"1910\" height=\"950\"></body></html>";
+        return "<html><body><img src=\"/Wallpaper.jpg\" width=\"1920\" height=\"950\"></body></html>";
     }
 }
