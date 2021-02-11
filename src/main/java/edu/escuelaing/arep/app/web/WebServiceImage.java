8ea3a15104949625431e5e7fc5fb79a59bee3e4b
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-11 15:24:53 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arep/app/web/WebServiceImage.java
+++ src/main/java/edu/escuelaing/arep/app/web/WebServiceImage.java
@@ -9,6 +9,6 @@ public class WebServiceImage {
      */
     @Web("/fondodepantalla.html")
     public static String showImage() {
-        return "<html><body><img src=\"/Wallpaper.jpg\" width=\"1900\" height=\"950\"></body></html>";
+        return "<html><title>Fondo de Pantalla</title><body><img src=\"/Wallpaper.jpg\" width=\"1900\" height=\"950\"></body></html>";
     }
 }
--- src/main/java/edu/escuelaing/arep/app/web/WebServiceJs.java
+++ src/main/java/edu/escuelaing/arep/app/web/WebServiceJs.java
@@ -9,7 +9,7 @@ public class WebServiceJs {
      */
     @Web("/agradecimiento.html")
     public static String returnHtmlWithJS(){
-        return "<html><head><script src=\"/agradecimiento.js\"></script></head>"
+        return "<html><title>Agradecimiento</title><head><script src=\"/agradecimiento.js\"></script></head>"
         		+ "<body style = \"background: url(https://www.setaswall.com/wp-content/uploads/2017/03/Artistic-Landscape-4K-Wallpaper-3840x2160.jpg) no-repeat ; background-size: 100% 100%;\">\r\n"
         		+ "</body>"
         		+ "</html>";
