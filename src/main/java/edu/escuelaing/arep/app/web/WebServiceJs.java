4b1760962dece13b3b381d67a3ce0269587f2bf5
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-11 15:18:26 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arep/app/web/WebServiceJs.java
+++ src/main/java/edu/escuelaing/arep/app/web/WebServiceJs.java
@@ -10,7 +10,7 @@ public class WebServiceJs {
     @Web("/agradecimiento.html")
     public static String returnHtmlWithJS(){
         return "<html><head><script src=\"/agradecimiento.js\"></script></head>"
-        		+ "<body style = \"background: url(https://i.pinimg.com/originals/19/3e/cb/193ecbf389570118d51e658cfefc9746.jpg) no-repeat ; background-size: 100% 111%;\">\r\n"
+        		+ "<body style = \"background: url(https://www.setaswall.com/wp-content/uploads/2017/03/Artistic-Landscape-4K-Wallpaper-3840x2160.jpg) no-repeat ; background-size: 100% 111%;\">\r\n"
         		+ "</body>"
         		+ "</html>";
     }
